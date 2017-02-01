552a99fc5fbce81a4a7a0dae94b3becc2ada1060
julian <julianl@live.com.ar>
2017-02-01 21:30:02 +0000

    Show article button to user log in

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -13,31 +13,26 @@ RSpec.feature "Listing Articles" do
 user: @john)
   end
   
-  scenario "A user list all articles" do
+  scenario "with articles created and user not signed in" do 
     visit "/"
-
-  expect(page).to have_content(@article1.title) 
-  expect(page).to have_content(@article1.body) 
-  expect(page).to have_content(@article2.title) 
-  expect(page).to have_content(@article2.body) 
-  expect(page).to have_link(@article1.title) 
-  expect(page).to have_link(@article2.title)
+    expect(page).to have_content(@article1.title) 
+    expect(page).to have_content(@article1.body) 
+    expect(page).to have_content(@article2.title) 
+    expect(page).to have_content(@article2.body) 
+    expect(page).to have_link(@article1.title) 
+    expect(page).to have_link(@article2.title) 
+    expect(page).not_to have_link("New Article")
   end
   
-  scenario "A user has no articles" do 
-    Article.delete_all
-
+  scenario "with articles created and user signed in" do 
+    login_as(@john)
     visit "/"
-
-    expect(page).not_to have_content(@article1.title)
-    expect(page).not_to have_content(@article1.body)
-    expect(page).not_to have_content(@article2.title)
-    expect(page).not_to have_content(@article2.body)
-    expect(page).not_to have_link(@article1.title)
-    expect(page).not_to have_link(@article2.title)
-
-    within ("h1#no-articles") do
-      expect(page).to have_content("No Articles Created")
-    end
+    expect(page).to have_content(@article1.title) 
+    expect(page).to have_content(@article1.body) 
+    expect(page).to have_content(@article2.title) 
+    expect(page).to have_content(@article2.body) 
+    expect(page).to have_link(@article1.title) 
+    expect(page).to have_link(@article2.title) 
+    expect(page).to have_link("New Article")
   end
 end
